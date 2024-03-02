edfddda6f1bb990acd2bb1e7e7a46eb02c1398ec
Arun Kumar R <aruntutter19@gmail.com>
2024-03-02 15:16:50 +0530

    created Navbar and styled

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
 import React from "react";
 import "./App.css";
+import Navbar from "./components/navbar/Navbar";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <Navbar />
+    </div>
+  );
 };
 
 export default App;
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,8 +1,23 @@
 import React from "react";
 import "./Navbar.css";
+import Logo from "../../assets/logo.png";
+import SearchIcon from "../../assets/search-icon.svg";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div className="navbar">
+      {/* Logo */}
+      <div className="logo">
+        <img src={Logo} alt="Search-Icon" />
+      </div>
+
+      {/* Search Bar */}
+      <div className="search">
+        <input placeholder="Search for movies" />
+        <img src={SearchIcon} alt="search icon" />
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
