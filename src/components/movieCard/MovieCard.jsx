8d0be881db01a328cdcae4a4293931158aa8d871
Arun Kumar R <aruntutter19@gmail.com>
2024-03-02 16:15:22 +0530

    created movie card and movie list to display the movies fetched from API

--- src/components/movieCard/MovieCard.jsx
+++ src/components/movieCard/MovieCard.jsx
@@ -1,8 +1,32 @@
 import React from "react";
 import "./MovieCard.css";
 
-const MovieCard = () => {
-  return <div>MovieCard</div>;
+const MovieCard = ({ title, poster_path, vote_average, overview }) => {
+  const img_Url = "https://image.tmdb.org/t/p/w500";
+
+  return (
+    <div className="movie-card">
+      <img src={`${img_Url}${poster_path}`} alt={title} />
+      <div className="movie-info">
+        <h3>{title}</h3>
+        <span className={getColor(vote_average)}>{vote_average}</span>
+      </div>
+      <div className="overview">
+        <h3>Overview</h3>
+        {overview}
+      </div>
+    </div>
+  );
 };
+
+function getColor(vote) {
+  if (vote >= 8) {
+    return "green";
+  } else if (vote >= 5) {
+    return "orange";
+  } else {
+    return "red";
+  }
+}
 
 export default MovieCard;
--- src/components/movieList/MovieList.jsx
+++ src/components/movieList/MovieList.jsx
@@ -1,8 +1,19 @@
 import React from "react";
 import "./MovieList.css";
+import MovieCard from "../movieCard/MovieCard";
 
-const MovieList = () => {
-  return <div>MovieList</div>;
+const MovieList = ({ movies }) => {
+  return (
+    <div className="main">
+      {movies.length > 0 ? (
+        movies.map((movie) => <MovieCard key={movie.id} {...movie} />)
+      ) : (
+        <div className="empty">
+          <h2>Search not found 🙁</h2>
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default MovieList;
